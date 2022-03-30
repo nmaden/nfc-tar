1a2264678193a986cbc9c29fba3e83232fd26cac
John Doe <abyl@kz.kz>
2022-03-30 17:16:40 +0500

    fixed main

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import VueToast from 'vue-toast-notification'
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 
-Vue.prototype.$API_VERSION = 'v1/'
+Vue.prototype.$API_VERSION = process.env.VUE_APP_API_VERSION;
 Vue.prototype.$API_VERSION_2 = process.env.VUE_APP_API_VERSION_2;
-Vue.prototype.$API_URL = 'http://127.0.0.1:8000/api/';
+Vue.prototype.$API_URL = 'https://api.kazaerospace.crocos.kz/api/';
 Vue.prototype.$API_MAIN = process.env.VUE_APP_API;
 Vue.use(VueToast);
 new Vue({
