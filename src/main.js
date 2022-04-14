1bfee6858ededcf3af895349d194700cc7a7ba80
John Doe <abyl@kz.kz>
2022-04-14 15:47:13 +0500

    added remove partners

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ Vue.prototype.$axios = axios;
 
 Vue.prototype.$API_VERSION = process.env.VUE_APP_API_VERSION;
 Vue.prototype.$API_VERSION_2 = process.env.VUE_APP_API_VERSION_2;
-Vue.prototype.$API_URL = 'https://api.kazaerospace.crocos.kz/api/';
+Vue.prototype.$API_URL = 'http://api.kazaerospace.crocos.kz/api/';
 Vue.prototype.$API_MAIN = process.env.VUE_APP_API;
 Vue.use(VueToast);
 
